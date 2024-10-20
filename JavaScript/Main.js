13561e9c02c4a1a8d1449d5544051ebe08acab57
Alex <54591924+AlexTheDev07@users.noreply.github.com>
2024-10-20 12:39:31 +0200

    Add files via upload

---
+++ JavaScript/Main.js
@@ -0,0 +1,48 @@
+function YouTube() {
+    window.open("")
+}
+
+function Discord() {
+    window.open("https://discord.gg/QsTVcpG2Tj")
+}
+
+function TikTok() {
+    window.open("")
+}
+
+function Twitter() {
+    window.open("https://x.com/MysticConst_RLX")
+}
+
+
+function Join() {
+    window.open("https://www.roblox.com/groups/32011927/Mystic-Construction#!/about")
+}
+
+function PetsBay() {
+    window.open("https://www.roblox.com/games/17458909777/Pets-Bay")
+}
+
+function WonderWorld() {
+    window.open("https://www.roblox.com/games/17536932840/Wonder-World-RP")
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Array med bakgrunnsbilder
+    const backgrounds = [
+        '/Main/Images/Background1.png',
+        '/Main/Images/Background2.png'
+    ];
+
+    // Forhåndslaste alle bakgrunnsbilder
+    backgrounds.forEach(function(image) {
+        const img = new Image();
+        img.src = image;
+    });
+
+    // Velg et tilfeldig bilde fra arrayen
+    const randomIndex = Math.floor(Math.random() * backgrounds.length);
+
+    // Sett bakgrunnsbildet på body-elementet
+    document.body.style.backgroundImage = `url(${backgrounds[randomIndex]})`;
+});
